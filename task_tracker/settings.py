1e2e0af6fc14e7bf1953dd1fd62c69dd2bd2cfba
Bogdan Olefir <bogdan@example.com>
2026-04-23 18:52:42 +0300

    fix: url routing and migrations

--- task_tracker/settings.py
+++ task_tracker/settings.py
@@ -67,5 +67,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/tasks/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
